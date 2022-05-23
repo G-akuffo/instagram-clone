63f95398dce82d66ea47ba0a9b01288c1ab28426
Gottfried Akuffo <gottfried.akuffo@gmail.com>
2022-05-23 22:58:49 +0000

    progress on post component

--- components/home/Post.js
+++ components/home/Post.js
@@ -1,7 +1,11 @@
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 import React from "react";
 import { Divider } from "react-native-elements";
 import { POSTS } from "./../../data/posts";
+import { AntDesign } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import { Octicons } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
 
 const Post = ({ post }) => {
   return (
@@ -10,6 +14,12 @@ const Post = ({ post }) => {
       <PostHeader post={post} />
       <Text style={{ color: "white" }}>{POSTS.user}</Text>
       <PostImage post={post} />
+      <View style={{ marginHorizontal: 15, marginTop: 10 }}>
+        <PostFooter />
+        <Likes post={post} />
+        <Caption post={post} />
+        <CommentsSection post={post} />
+      </View>
     </View>
   );
 };
@@ -26,11 +36,11 @@ const PostHeader = ({ post }) => {
     >
       <View style={{ flexDirection: "row", alignItems: "center" }}>
         <Image source={{ uri: post.profile_picture }} style={styles.post} />
-        <Text style={{ color: "white", marginLeft: 5, fontWeight: 700 }}>
+        <Text style={{ color: "white", marginLeft: 5, fontWeight: "700" }}>
           {post.user}
         </Text>
       </View>
-      <Text style={{ color: "white", fontWeight: 900 }}>...</Text>
+      <Text style={{ color: "white", fontWeight: "900" }}>...</Text>
     </View>
   );
 };
@@ -49,6 +59,52 @@ const PostImage = ({ post }) => (
   </View>
 );
 
+const PostFooter = () => (
+  <View style={{ flexDirection: "row" }}>
+    <View style={styles.leftFooterIconsView}>
+      <TouchableOpacity>
+        <AntDesign name="hearto" size={30} color="white" />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.bubbleIcon}>
+        <Ionicons name="chatbubble-outline" size={30} color="white" />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.shareIcon}>
+        <Octicons name="paper-airplane" size={30} color="white" />
+      </TouchableOpacity>
+    </View>
+    <View style={{ flex: 1, alignItems: "flex-end" }}>
+      <TouchableOpacity>
+        <Feather name="bookmark" size={30} color="white" />
+      </TouchableOpacity>
+    </View>
+  </View>
+);
+
+const Likes = ({ post }) => (
+  <View style={{ flexDirection: "row", marginTop: 4 }}>
+    <Text style={{ color: "white", fontWeight: "600" }}>
+      {post.likes.toLocaleString("en")} likes
+    </Text>
+  </View>
+);
+
+const Caption = ({ post }) => (
+  <View>
+    <Text style={{ color: "white" }}>
+      <Text style={{ fontWeight: "600" }}>{post.user}</Text>
+      <Text> {post.caption}</Text>
+    </Text>
+  </View>
+);
+
+const CommentsSection = ({ post }) => (
+  <Text style={{ color: "grey" }}>
+    View{post.comments.length > 1 ? " all" : ""} {post.comments.length}
+    {""}
+    {post.comments.length > 1 ? "comments" : "comment"}
+  </Text>
+);
+
 const styles = StyleSheet.create({
   post: {
     width: 40,
@@ -58,6 +114,26 @@ const styles = StyleSheet.create({
     borderWidth: 3,
     borderColor: "#ff8501",
     justifyContent: "space-evenly",
+  },
+
+  footerIcons: {
+    width: 33,
+    height: 33,
+  },
+
+  leftFooterIconsView: {
+    flexDirection: "row",
+    width: "32%",
+    justifyContent: "space-between",
+  },
+
+  bubbleIcon: {
+    transform: [{ rotate: "300deg" }],
+  },
+
+  shareIcon: {
+    transform: [{ rotate: "320deg" }],
+    marginLeft: 4,
   },
 });
 
--- data/posts.js
+++ data/posts.js
@@ -6,15 +6,28 @@ export const POSTS = [
       "https://pbs.twimg.com/media/E9amnSLWQAAHEfh?format=jpg&name=medium",
     user: USERS[6].user,
     likes: 7870,
-    caption: "train ride to hogwarts",
+    caption: "wedding guest",
     profile_picture: USERS[6].image,
+    comments: [
+      {
+        user: "theqazman",
+        comment:
+          "this is nice. very nice. so nice. fanice, so nice, nice, nice",
+      },
+      {
+        user: "amaanath.dev",
+        comment:
+          "something something I wanna see this on multiple lines so I will type just anything to make it that",
+      },
+    ],
   },
   {
     imageUrl:
       "https://pbs.twimg.com/media/FRs7bejWYAI9kX7?format=jpg&name=large",
     user: USERS[0].user,
-    likes: 9983,
-    caption: "wedding guest",
+    likes: 983,
+    caption:
+      "best album. this album brings back so many of my teenage memories",
     profile_picture: USERS[0].image,
   },
 ];
